7a2f683fb7158c6b4ef06c1066be34218751231a
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-04 20:21:12 +0900

    SVM xor problem
    SVM으로 xor문제 코딩

---
+++ 정보전산원 수업/Sklearn/SVM/svm_xor.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.svm import SVC
+
+# XOR
+xor_input = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]]
+
+# 데이터 분류
+xor_df = pd.DataFrame(xor_input)
+xor_data = xor_df.iloc[:, 0:2]
+# print(xor_data)
+xor_label = xor_df.iloc[:, 2]
+# print(xor_label)
+
+# 데이터 학습과 예측하기
+clf = SVC()
+clf.fit(xor_data, xor_label)
+pre = clf.predict(xor_data)
